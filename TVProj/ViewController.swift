39b2372e9ea9b4b69999bffba4bded8f7dbbb81b
Eugene <evgen29031999@bk.ru>
2021-11-19 21:47:11 +0300

    Реализация на StoryBoard

--- TVProj/ViewController.swift
+++ TVProj/ViewController.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //var myTableView = UITableView()
-    //let identifier = "MyCell"
-    //var array = ["1","2","3","4","5"]
-    
     
     @IBOutlet weak var myTableView: UITableView!
     
@@ -24,60 +20,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //createTable()
         
     }
-    
-    /*
-    func createTable(){
-        self.myTableView = UITableView(frame: view.bounds, style: .plain)
-        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
-        
-        self.myTableView.delegate = self
-        self.myTableView.dataSource = self
-        
-        myTableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        view.addSubview(myTableView)
-    }
-    
-    //UITableViewDataSource
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
-        
-        let number = array[indexPath.row]
-        
-        cell.textLabel?.text = number
-        
-        return cell
-    }
-    
-    
-    //UITableViewDelegate
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 70.0
-    }
-    
-    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
-        print("Accesory path = ", indexPath)
-        
-        let ounerCell = tableView.cellForRow(at: indexPath)
-        
-        print("Cell title =", ounerCell?.textLabel?.text ?? "nil")
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let number = array[indexPath.row]
-        print(number)
-    }
-     */
-
 }
 
 //commit!
@@ -97,7 +41,5 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
         return cell
         
     }
-    
-    
 }
 
